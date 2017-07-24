ff7ec7904249f47e77c98050b8e6b51546bf8c06
GreatestAlly <jwang148@gmail.com>
2017-07-24 14:37:34 -0400

    tile to create a neutral zone

---
+++ Items/Placeable/NeutralityStatue.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace tgmod.Items.Placeable
+{
+    class NeutralityStatue : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Neutrality Statue");
+            Tooltip.SetDefault("Creates a faction-free zone.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.consumable = true;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.rare = 4;
+            item.value = 10000;
+            item.createTile = mod.TileType<Tiles.NeutralityTile>();
+            item.maxStack = 99;
+            item.width = 18;
+            item.height = 34;
+            item.stack = 1;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SunplateBlock, 50);
+            recipe.AddIngredient(ItemID.Hellstone, 50);
+            recipe.AddTile(TileID.SkyMill);
+            recipe.AddTile(TileID.Hellforge);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/NeutralityTile.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ObjectData;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace tgmod.Tiles
+{
+    class NeutralityTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = false;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Neutrality Statue");
+            AddMapEntry(new Color(144, 148, 144), name);
+            dustType = 11;
+            disableSmartCursor = true;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 48, mod.ItemType("ClearEventFurniture"));
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer)
+        {
+            Player player = Main.LocalPlayer;
+            player.GetModPlayer<tgplayer>().inNeutralZone = true;
+            base.NearbyEffects(i, j, closer);
+        }
+    }
+}
